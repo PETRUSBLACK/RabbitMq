f91ef145942b7438649d9771abce8e6192b6521c
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-03-13 17:45:01 +0100

    applying pub-sub => this populate message to all the consumer

---
+++ pub-sub/receive.py
@@ -0,0 +1,23 @@
+
+import pika
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(queue=queue_name, exchange='logs')
+
+print(' [x] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, boday):
+    print(" [x] %r" % body)
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_back=True)
+
+channel.start_consuming()
